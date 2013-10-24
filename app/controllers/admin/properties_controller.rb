404484540cc61a58a5fefed1c2140685a922d451
Jean Brito <jeanfbrito@gmail.com>
2013-10-24 21:00:38 -0200

    finish property form

--- app/controllers/admin/properties_controller.rb
+++ app/controllers/admin/properties_controller.rb
@@ -1,10 +1,6 @@
 class Admin::PropertiesController < ApplicationController
 	layout "admin" 
 
-  def property_params
-    params.require(:property).permit(:address_name, :value)
-  end
-
   def index
     @properties = Property.all
     respond_to do |format|
@@ -22,7 +18,7 @@ class Admin::PropertiesController < ApplicationController
   end
 
   def create
-    @property = Property.new params.require(:property).permit(:description, :value)
+    @property = Property.new(person_params)
 
     respond_to do |format|
       if @property.save
@@ -45,7 +41,7 @@ class Admin::PropertiesController < ApplicationController
 
     respond_to do |format|
       if @property.update_attributes(property_params)
-        format.html { redirect_to [:admin, @property], notice: 'Property was successfully updated.' }
+        format.html { redirect_to edit_admin_property_path(@property), notice: 'Property was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -60,5 +56,30 @@ class Admin::PropertiesController < ApplicationController
       format.json { render json: @property }
     end
   end
+
+  private
+    # Using a private method to encapsulate the permissible parameters is just a good pattern
+    # since you'll be able to reuse the same permit list between create and update. Also, you
+    # can specialize this method with per-user checking of permissible attributes.
+    def property_params
+      params.require(:property).permit( :address_name, 
+                                        :address_number,
+                                        :address_complement,
+                                        :address_reference,
+                                        :district_id,
+                                        :privative_area,
+                                        :constructed_area,
+                                        :total_area,
+                                        :terrain_area,
+                                        :description,
+                                        :bedrooms,
+                                        :suits,
+                                        :bathrooms,
+                                        :garages,
+                                        :furnished,
+                                        :construction_year,
+                                        :sell_price,
+                                        :client_price)
+    end
 
 end
